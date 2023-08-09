c5def2c62d16e42548e89dd5f473924ae3dfed97
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-08-10 07:55:50 +0800

    Test and function

--- src/__tests__/getAttacks.js
+++ src/__tests__/getAttacks.js
@@ -1,5 +1,117 @@
 import getAttacks from '../getAttacks';
 
-test('Test', () => {
-  expect(getAttacks({})).toEqual({});
+test.each([
+  [
+    {
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 50,
+      level: 3,
+      attack: 25,
+      defence: 25,
+      special: [
+        {
+          id: 8,
+          name: 'Двойной выстрел',
+          icon: 'http://...',
+          description: 'Двойной выстрел наносит двойной урон',
+        },
+        {
+          id: 9,
+          name: 'Нокаутирующий удар',
+          icon: 'http://...',
+        },
+      ],
+    },
+    [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        description: 'Описание недоступно',
+      },
+    ],
+  ],
+  [
+    {
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 50,
+      level: 3,
+      attack: 25,
+      defence: 25,
+      special: [
+        {
+          id: 8,
+          name: 'Двойной выстрел',
+          icon: 'http://...',
+          description: 'Двойной выстрел наносит двойной урон',
+        },
+        {
+          id: 9,
+          name: 'Нокаутирующий удар',
+          icon: 'http://...',
+          description: 'Нокаутирующий удар парализует противника',
+        },
+      ],
+    },
+    [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        description: 'Нокаутирующий удар парализует противника',
+      },
+    ],
+  ],
+  [
+    {
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 50,
+      level: 3,
+      attack: 25,
+      defence: 25,
+      special: [
+        {
+          id: 8,
+          name: 'Двойной выстрел',
+          icon: 'http://...',
+        },
+        {
+          id: 9,
+          name: 'Нокаутирующий удар',
+          icon: 'http://...',
+        },
+      ],
+    },
+    [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Описание недоступно',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        description: 'Описание недоступно',
+      },
+    ],
+  ],
+])('Test', (obj, result) => {
+  expect(getAttacks(obj)).toEqual(result);
 });
--- src/getAttacks.js
+++ src/getAttacks.js
@@ -1,3 +1,13 @@
 export default function getAttacks(obj) {
-  return obj;
+  const { special } = obj;
+  const result = [];
+  special.forEach((element) => {
+    const {
+      id, name, icon, description = 'Описание недоступно',
+    } = element;
+    result.push({
+      id, name, icon, description,
+    });
+  });
+  return result;
 }
